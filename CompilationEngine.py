4120987cc2b914199ecd12610b50066c17051295
Nadav Geva <nadavgeva4@gmail.com>
2016-06-06 14:26:36 +0300

    Add compilation engine skeleton

--- CompilationEngine.py
+++ CompilationEngine.py
@@ -1,3 +1,4 @@
+import sys
 
 class CompilationEngine:
 	'''A compilation engine for the Jack programming language'''
@@ -12,9 +13,66 @@ class CompilationEngine:
 
 	def compile_class(self):
 		'''Compile a class block'''
-		pass
+		self.open_tag('class')
 
-	def write_tag(self, token):
+		token = self.tokenizer.advance()
+		if token is None or token != ('keyword', 'class'):
+			print('Error compiling class, missing class token')
+			sys.exit(1)
+
+		self.terminal_tag(token)
+
+		token = self.tokenizer.advance()
+		if token is None or token.type != 'identifier':
+			print('Error compiling class, missing or invalid class name')
+			sys.exit(1)
+
+		self.terminal_tag(token)
+		class_name = token.value
+
+		token = self.tokenizer.advance()
+		if token is None or token != ('symbol','{'):
+			print('Error compiling class, missing opening parenthesis')
+			sys.exit(1)
+
+		self.terminal_tag(token)
+
+		self.compile_class_vars(class_name)
+		self.compile_class_subroutines(class_name)
+
+		token = self.tokenizer.advance()
+		if token is None or token != ('symbol','}'):
+			print('Error compiling class, missing closing parenthesis')
+			sys.exit(1)
+
+		self.terminal_tag(token)
+		self.close_tag('class')
+
+	def compile_class_vars(self, class_name):
+		'''Compile the class variable declarations'''
+		self.open_tag('classVarDec')
+		# TODO
+		self.close_tag('classVarDec')
+
+	def compile_class_subroutines(self, class_name):
+		'''Compile the class subroutines'''
+		self.open_tag('subroutineDec')
+		# TODO
+		self.close_tag('subroutineDec')
+
+	def open_tag(self, name):
+		'''Open a containing tag, and indent from now on'''
+		self.ostream.write(' '*self.indent)
+		self.ostream.write('<{}>\n'.format(name))
+		self.indent += 2
+
+	def close_tag(self, name):
+		'''Close an open tag, and decrease the indentation level'''
+		self.indent -= 2
+		self.ostream.write(' '*self.indent)
+		self.ostream.write('</{}>\n'.format(name))
+
+	def terminal_tag(self, token):
 		'''Write a tag to the ostream'''
 		self.ostream.write(' '*self.indent)
 		self.ostream.write(
